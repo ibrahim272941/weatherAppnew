f747586d2ccf454527456c22e8deec1568cb8e37
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2022-09-28 21:46:47 +0200

    Info vom Land wurde hinzufügen

--- app.js
+++ app.js
@@ -16,37 +16,75 @@ form.onsubmit = (e) => {
 };
 
 const getWeatherData = async () => {
-  const cityCard = document.createElement('div');
-  cityCard.classList.add('city');
   let input = formInput.value;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=${weatherType}`;
   try {
+    //!----getCityWeatherInfo--------//////
+    const cityCard = document.createElement('div');
+    cityCard.classList.add('city');
+    const countryInfo = document.createElement('div');
+    countryInfo.classList.add('country');
     const res = await fetch(url);
     const data = await res.json();
     const { main, name, sys, weather } = data;
+    console.log(data);
+
+    //!------getCountryInfo---------//////
+    const response = await fetch(
+      `https://restcountries.com/v3.1/alpha/${sys.country}`
+    );
+
+    const data2 = await response.json();
+
+    const {
+      name: { official },
+      flags: { png },
+      capital,
+      currencies,
+      region,
+      languages,
+      population,
+    } = data2[0];
+    console.log(data2[0]);
     const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@4x.png`;
-    console.log(iconUrl);
     cityCard.innerHTML = ` <div class="head">
-                <h2 class="cityName"><span>${name}</span><sup>${
-      sys.country
-    }</sup>
-                </h2>
-                <div class="closeIcon">X</div>
-            </div>
+    <h2 class="cityName"><span>${name}</span><sup>${sys.country}</sup>
+    </h2>
+    <div class="closeIcon">X</div>
+    </div>
+    
+    <div class="cityTemp">${Math.round(
+      parseFloat(main.temp)
+    )}<sup>°C</sup></div>
+      
+      <img class="cityIcon" src="${iconUrl}">
+      <p>${weather[0].description}</p>
+      `;
+    countryInfo.innerHTML = ` 
+                    <img src="${png}" alt="" class="countryImg">
+                    <div class="countryData">
+                        <h4 class="countryName">
+                            ${official}
+                        </h4>
+                        <h4>${capital[0]}</h4>
+                        <h5 class="countryRegion">${region}</h5>
+                        <h5 class="countryCapital">${
+                          Object.values(currencies)[0].name
+                        }</h5>
+                        <h4 class="countryPop">${(
+                          +population / 1_000_000
+                        ).toFixed(1)}M People</h4>
+                        <h4 class="countryLang">${Object.values(
+                          languages
+                        )}</h4>`;
 
-            <div class="cityTemp">${Math.round(
-              parseFloat(main.temp)
-            )}<sup>°C</sup></div>
-
-            <img class="cityIcon" src="${iconUrl}">
-            <p>${weather[0].description}</p>`;
-
+    cityCard.appendChild(countryInfo);
     resultArea.appendChild(cityCard);
     warning.innerText = '';
     form.reset();
     form.focus();
   } catch (error) {
-    warning.innerText = `Die Stadt , die Sie gesucht haben , wurde nicht gefunden${error}`;
+    warning.innerText = `Die Stadt , die Sie gesucht haben , wurde nicht gefunden`;
   }
 };
 const cardDelete = (city) => {
@@ -58,9 +96,10 @@ resultArea.onclick = (e) => {
     cardDelete(e.target);
   }
 };
+
 const findMyState = (e) => {
   const success = (position) => {
-    const remain = 0.1706794824218747;
+    const remain = 0.17;
     const latitude = position.coords.latitude + remain;
     const longitude = position.coords.longitude;
 
@@ -85,7 +124,9 @@ const getCurrentCityWeather = async (e) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${e}&appid=${apiKey}&units=${weatherType}`;
   const res = await fetch(url);
   const data = await res.json();
+
   const { main, name, sys, weather } = data;
+
   const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@4x.png`;
   currentCity.innerHTML = `<div class="cityCurrent">
             <div>${date}</div>
@@ -104,3 +145,43 @@ const getCurrentCityWeather = async (e) => {
             <img  class="cityIcon" src="${iconUrl}">
             <p>${weather[0].description}</p>`;
 };
+
+// const getCountryDataByName = async (a) => {
+//   const countryInfo = document.createElement('div');
+//   countryInfo.classList.add('country');
+//   const response = await fetch(`https://restcountries.com/v3.1/alpha/${a}`);
+//   if (!response.ok) throw new Error(`something is wrong! ${response.status}`);
+//   const data = await response.json();
+
+//   const {
+//     name: { official },
+//     flags: { png },
+//     capital,
+//     currencies,
+//     region,
+//     languages,
+//     population,
+//   } = data[0];
+
+//   countryInfo.innerHTML = `
+//                     <img src="${png}" alt="" class="countryImg">
+//                     <div class="countryData">
+//                         <h4 class="countryName">
+//                             ${official}
+//                         </h4>
+//                         <h6>${capital[0]}</h6>
+//                         <h5 class="countryRegion">${region}</h5>
+//                         <p class="countryCapital">${
+//                           Object.values(currencies)[0].name
+//                         }</p>
+//                         <p class="countryPop">${(
+//                           +population / 1_000_000
+//                         ).toFixed(1)}M People</p>
+//                         <div class="countryLang">${Object.values(
+//                           languages
+//                         )}</div>
+//                    `;
+//   //getWeatherData(countryInfo);
+//   return countryInfo;
+// };
+// console.log(getCountryDataByName('DE').then((e) => e));
